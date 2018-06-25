b979b597f9252ecc3f4b20437dbcc1a525b47b97
michitaro.koike <michitaro.nike@gmail.com>
2018-06-25 09:17:11 +0900

    handle http error

--- src/fits.ts
+++ src/fits.ts
@@ -27,8 +27,13 @@ function ajax(url: string) {
     return new Promise<ArrayBuffer>((resolve, reject) => {
         const xhr = new XMLHttpRequest()
         xhr.open('GET', url)
-        xhr.onload = () => resolve(xhr.response)
-        xhr.onerror = (error) => reject(error)
+        xhr.addEventListener('load', () => {
+            if (xhr.status == 200)
+                resolve(xhr.response)
+            else
+                reject(`status=${xhr.status}`)
+        })
+        xhr.addEventListener('error', e => reject(e))
         xhr.responseType = 'arraybuffer'
         xhr.send()
     })
